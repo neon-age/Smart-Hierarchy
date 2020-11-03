099ebaa3ec02d31120c8f4962dc142e786fe42e7
neon-age <alexander.neonage@gmail.com>
2020-11-03 20:37:51 +0700

    Detach to parent instead of scene root

--- Base/DetachOnBuildProcess.cs
+++ Base/DetachOnBuildProcess.cs
@@ -11,44 +11,40 @@ namespace AV.Hierarchy
     internal class DetachOnBuildProcess : IProcessSceneWithReport
     {
         public int callbackOrder { get; }
+        
         public void OnProcessScene(Scene scene, BuildReport report)
         {
-            var isEditor = Application.isEditor;
             var sceneRoots = scene.GetRootGameObjects();
             foreach (var root in sceneRoots)
             {
-                foreach (var folder in root.GetComponentsInChildren<Folder>(true))
+                var folders = root.GetComponentsInChildren<Folder>(true);
+                
+                foreach (var folder in folders)
                 {
-                    var transform = folder.transform;
-                    
-                    if (!isEditor)
-                    {
-                        transform.DetachChildren();
-                        Object.DestroyImmediate(folder);
-                        continue;
-                    }
-
-                    var roots = new Transform[transform.childCount];
-                    var siblings = new int[roots.Length];
-                    var folderSibling = transform.GetSiblingIndex();
-
-                    for (int i = 0; i < roots.Length; i++)
-                    {
-                        roots[i] = transform.GetChild(i);
-                        siblings[i] = transform.GetSiblingIndex();
-                    }
-
-                    //transform.DetachChildren();
-                    
-                    for (int i = roots.Length - 1; i >= 0; i--)
-                    {
-                        roots[i].SetParent(null);
-                        roots[i].SetSiblingIndex(siblings[i]);
-                    }
-                    
-                    transform.SetSiblingIndex(folderSibling);
+                    DetachRootChildren(folder.transform);
                 }
             }
+        }
+
+        private void DetachRootChildren(Transform transform)
+        {
+            var roots = new Transform[transform.childCount];
+            var siblings = new int[roots.Length];
+            var folderSibling = transform.GetSiblingIndex();
+
+            for (int i = 0; i < roots.Length; i++)
+            {
+                roots[i] = transform.GetChild(i);
+                siblings[i] = transform.GetSiblingIndex();
+            }
+
+            for (int i = roots.Length - 1; i >= 0; i--)
+            {
+                roots[i].SetParent(transform.parent);
+                roots[i].SetSiblingIndex(siblings[i]);
+            }
+                    
+            transform.SetSiblingIndex(folderSibling);
         }
     }
 }
