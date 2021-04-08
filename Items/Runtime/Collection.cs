964d1ce0df11ec310e3949ed2697a0b9f4670cf6
neon-age <alexander.neonage@gmail.com>
2021-04-08 20:43:52 +0700

    Runtime assembly

--- Items/Runtime/Collection.cs
+++ Items/Runtime/Collection.cs
@@ -3,7 +3,7 @@
 namespace AV.Hierarchy
 {
     [AddComponentMenu("GameObject/Collection")]
-    internal class Collection : MonoBehaviour
+    public class Collection : MonoBehaviour
     {
         [Tooltip("Will skip collection stripping during scene process.\n" +
                  "Use only when you know that transform overhead is doable.")]
---
+++ Items/Runtime/ColorTag.cs
@@ -0,0 +1,36 @@
+﻿
+namespace AV.Hierarchy
+{
+    public enum ColorTag
+    {
+        // https://chir.ag/projects/name-that-color/
+        /// <summary>Silver / Emperor. UI</summary>
+        Gray, 
+        /// <summary>
+        /// Burning Orange / Milano Red. Navigation
+        /// </summary>
+        Red,
+        /// <summary>
+        /// Amber / Buddha Gold. Audio
+        /// </summary>
+        Orange,
+        /// <summary>
+        /// Green Yellow / Sea Green. Environment
+        /// </summary>
+        Green,
+        /// <summary>
+        /// Aquamarine / Surfie Green. Animation
+        /// </summary>
+        Aquamarine,
+        /// <summary>
+        /// Malibu / Denim. Rendering
+        /// </summary>
+        Blue,
+        /// <summary>
+        /// Portage / Royal Purple. Sprites
+        /// </summary>
+        Purple,
+        /// <summary>Light Orchid / Cerise. Constraints</summary> 
+        Pink, 
+    }
+}
