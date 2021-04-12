680e7dafd42320ab0819a05e46933d24eac299f6
neon-age <alexander.neonage@gmail.com>
2021-04-12 19:57:59 +0700

    Revert "Runtime assembly"
    This reverts commit f5d30415e9e507b540331721c8d36b82ca4f7362.

--- Items/Collections/Collection.cs
+++ Items/Collections/Collection.cs
@@ -3,7 +3,7 @@
 namespace AV.Hierarchy
 {
     [AddComponentMenu("GameObject/Collection")]
-    public class Collection : MonoBehaviour
+    internal class Collection : MonoBehaviour
     {
         [Tooltip("Will skip collection stripping during scene process.\n" +
                  "Use only when you know that transform overhead is doable.")]
