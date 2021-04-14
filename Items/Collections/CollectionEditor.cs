f2bbcc9568c865f520fcb1f3caba91f40c46a040
neon-age <alexander.neonage@gmail.com>
2021-04-14 15:35:48 +0700

    Rewrite Collection header injection to UIElements (fixes #28)

--- Items/Collections/CollectionEditor.cs
+++ Items/Collections/CollectionEditor.cs
@@ -7,9 +7,11 @@ using System.Reflection;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.UIElements;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UIElements;
 using static UnityEditor.EditorGUIUtility;
+using Object = UnityEngine.Object;
 
 namespace AV.Hierarchy
 {
@@ -17,47 +19,21 @@ namespace AV.Hierarchy
     [CanEditMultipleObjects]
     internal class CollectionEditor : Editor
     {
-        // TODO: Remove all this reflection junk and use UI Elements instead.
-        private static class Reflected
-        {
-            public static Type gameObjectInspectorType;
-            public static Type inspectorWindowType;
-            // Need this to hide GameObject editor in inspector
-            public static FieldInfo hideInspector;
-            
-            public static PropertyInfo getInspectorTracker;
-            public static Func<object[]> getAllInspectorWindows;
-
-            static Reflected()
-            {
-                gameObjectInspectorType = typeof(Editor).Assembly.GetType("UnityEditor.GameObjectInspector");
-                inspectorWindowType = typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow");
-
-                var getAllInspectorsMethod = inspectorWindowType.GetMethod("GetAllInspectorWindows",
-                    BindingFlags.NonPublic | BindingFlags.Static);
-                getAllInspectorWindows =
-                    Expression.Lambda<Func<object[]>>(Expression.Call(null, getAllInspectorsMethod)).Compile();
-                getInspectorTracker = inspectorWindowType.GetProperty("tracker", 
-                    BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-                hideInspector =
-                    typeof(Editor).GetField("hideInspector",
-                        BindingFlags.NonPublic | BindingFlags.Instance);
-            }
-        }
-        
+        private static Type inspectorWindowType = typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow");
         private static StyleSheet helpBoxStyle;
-
 
         private SerializedProperty keepHierarchy;
         private SerializedProperty colorTag;
         
-        private new Transform target;
-        private new Transform[] targets;
-        // TODO: Local-Hierarchy
-        private GameObject[] children;
+        private new Collection target => base.target as Collection;
+        private GameObject gameObject => target.gameObject;
+        private Transform[] transforms = new Transform[0];
 
         private VisualElement root;
         private VisualElement editorsList;
+        private VisualElement transformEditor;
+        private VisualElement collectionEditor;
+        private VisualElement addComponentButton;
         private VisualElement componentsWarning;
 
         private int editorsCount;
@@ -69,72 +45,6 @@ namespace AV.Hierarchy
             // Reselect to update folder header
             var instanceIDs = Selection.instanceIDs;
             Selection.instanceIDs = instanceIDs;
-        }
-        
-        private void OnEnable()
-        {
-            target = (base.target as Collection).transform;
-            targets = new Transform[base.targets.Length];
-            for (int i = 0; i < targets.Length; i++)
-                targets[i] = (base.targets[i] as Collection).transform;
-
-            children = new GameObject[target.transform.childCount];
-            for (int i = 0; i < target.childCount; i++)
-            {
-                children[i] = target.GetChild(i).gameObject;
-            }
-
-            foreach (var target in targets)
-            {
-                if((target.hideFlags & HideFlags.HideInInspector) == 0)
-                    target.hideFlags |= HideFlags.HideInInspector;
-            }
-
-            SetGameObjectInspectorHidden(true);
-        }
-
-        private void OnDestroy()
-        {
-            foreach (var target in targets)
-            {
-                if (target == null)
-                    return;
-                    
-                // Show transform when folder component is removed
-                if (!target.TryGetComponent<Collection>(out _))
-                    target.hideFlags ^= HideFlags.HideInInspector;
-            }
-        }
-        
-        private void SetGameObjectInspectorHidden(bool hide)
-        {
-            foreach (var inspector in Reflected.getAllInspectorWindows())
-            {
-                var tracker = Reflected.getInspectorTracker.GetValue(inspector, null) as ActiveEditorTracker;
-
-                for (var i = 1; i < tracker.activeEditors.Length; i++)
-                {
-                    var editor = tracker.activeEditors[i];
-                    
-                    if (editor == null)
-                        continue;
-                    
-                    if (!(editor.target is Collection folder))
-                        continue;
-                    
-                    foreach (var gameObjectInspector in Resources.FindObjectsOfTypeAll(Reflected.gameObjectInspectorType))
-                    {
-                        var goEditor = gameObjectInspector as Editor;
-                        var gameObject = goEditor.target as GameObject;
-
-                        // Hide GameObject inspector only for folders
-                        if (folder.gameObject == gameObject)
-                        {
-                            Reflected.hideInspector.SetValue(gameObjectInspector, hide);
-                        }
-                    }
-                }
-            }
         }
 
         public override VisualElement CreateInspectorGUI()
@@ -157,69 +67,124 @@ namespace AV.Hierarchy
             var keepHierarchyField = new PropertyField(keepHierarchy) { tooltip = keepHierarchy.tooltip };
             root.Add(keepHierarchyField);
             
-            root.RegisterCallback<ChangeEvent<bool>>(_ => SetComponentsDisabled(!keepHierarchy.boolValue));
-            root.RegisterCallback<AttachToPanelEvent>(_ => OnInspectorInitialize());
+            // Needs to be called before OnEditorsListChange
+            ShowTransformComponent();
+            
+            root.RegisterCallback<ChangeEvent<bool>>(_ => AlternateComponentsGUI(!keepHierarchy.boolValue));
+            root.RegisterCallback<AttachToPanelEvent>(OnAttachRootToInspector);
             
             return root;
         }
 
-        private void OnInspectorInitialize()
+        private void OnAttachRootToInspector(AttachToPanelEvent _)
         {
+            root.UnregisterCallback<AttachToPanelEvent>(OnAttachRootToInspector);
+            
         #if UNITY_2019_3 || UNITY_2019_4
-            var inspectors = Resources.FindObjectsOfTypeAll(Reflected.inspectorWindowType);
+            var inspectors = Resources.FindObjectsOfTypeAll(inspectorWindowType);
             foreach (EditorWindow inspector in inspectors)
                 inspector.SetAntiAliasing(8);
         #endif
 
-            root.Query<Label>().ForEach(label => label.style.width = 160);
-        
             editorsList = root.parent.parent.parent;
             editorsList.RegisterCallback<GeometryChangedEvent>(OnEditorsListChange);
         }
 
-        private void OnDisable()
+        private void ShowTransformComponent()
+        {
+            if (transforms.Length == targets.Length)
+                return;
+            
+            transforms = new Transform[targets.Length];
+            
+            for (int i = 0; i < targets.Length; i++)
+                transforms[i] = (targets[i] as Collection).transform;
+            
+            foreach (var transform in transforms)
+                if (transform)
+                    // hideFlags &= HideFlags.HideInInspector; doesn't work in 2019.4?
+                    transform.hideFlags = HideFlags.None;
+        }
+
+        private void OnDestroy()
         {
             editorsList?.UnregisterCallback<GeometryChangedEvent>(OnEditorsListChange);
+            editorsList?.Query(className: "element-dimmer").ForEach(x => x.RemoveFromHierarchy());
+            
+            // Custom header and editor lasts after Remove Component
+            collectionEditor?.RemoveFromHierarchy();
+            
+            addComponentButton?.SetEnabled(true);
         }
 
         private void OnEditorsListChange(GeometryChangedEvent evt)
         {
             if (editorsCount == editorsList.childCount)
                 return;
-                    
             editorsCount = editorsList.childCount;
-            SetComponentsDisabled(!keepHierarchy.boolValue);
+            
+            root.Query<Label>().ForEach(label => label.style.width = 160);
+
+            AlternateComponentsGUI(!keepHierarchy.boolValue);
         }
 
-        private void SetComponentsDisabled(bool disabled)
+        // TODO: Change header icon
+        // TODO: Add color bar button
+        // TODO: Add name field
+        // TODO: Add tag / layer / static popups that will affect root or all children  
+        protected override void OnHeaderGUI()
         {
-            var componentsByName = target.GetComponents<Component>().Where(c => c != null).ToDictionary(ObjectNames.GetInspectorTitle);
+            if (target == null)
+                return;
+            base.OnHeaderGUI();
+        }
+
+        private void AlternateComponentsGUI(bool isDisabled)
+        {
             var componentsCount = 0;
+            var editorsByName = target.GetComponents<Component>().Where(x => x != null).Cast<Object>().ToDictionary(ObjectNames.GetInspectorTitle);
+           
+            editorsByName.Add(ObjectNames.GetInspectorTitle(gameObject), gameObject);
 
             foreach (var editor in editorsList.Children())
             {
                 var header = editor[0];
-                var componentName = header.name;
+                var headerName = header.name;
                 
-                if (componentName.EndsWith("Header"))
-                    componentName = componentName.Remove(componentName.Length - 6, 6);
+                if (headerName.EndsWith("Header"))
+                    headerName = headerName.Remove(headerName.Length - 6, 6);
                 
-                if (componentsByName.TryGetValue(componentName, out var component))
+                if (editorsByName.TryGetValue(headerName, out var obj))
                 {
-                    if (component is Collection || component is Transform)
+                    if (obj is Collection collection)
+                    {
+                        collectionEditor = editor;
+                        
+                        for (int i = 0; i < componentsCount + 1; i++)
+                            ComponentUtility.MoveComponentUp(collection);
+                        
+                        collectionEditor.Query($"{headerName}Header").First().RemoveFromHierarchy();
+                        collectionEditor.Insert(0, new IMGUIContainer(OnHeaderGUI));
+                        
                         continue;
+                    }
+
+                    if (obj is Transform || obj is GameObject)
+                    {
+                        editor.RemoveFromHierarchy();
+                        return;
+                    }
 
                     componentsCount++;
-                    SetDisabledGroup(editor, disabled);
+                    SetElementDimmed(editor, isDisabled);
                 }
             }
 
             if (componentsCount == 0)
-                disabled = false;
+                isDisabled = false;
 
-            var componentButton = editorsList.parent.Query(className: "unity-inspector-add-component-button").First();
-            
-            componentButton.SetEnabled(!disabled);
+            addComponentButton = editorsList.parent.Query(className: "unity-inspector-add-component-button").First();
+            addComponentButton.SetEnabled(!isDisabled);
 
 
             if (componentsWarning == null)
@@ -241,34 +206,34 @@ namespace AV.Hierarchy
                 });
             }
 
-            if (disabled)
+            if (isDisabled)
                 root.Add(componentsWarning);
             else
                 componentsWarning.RemoveFromHierarchy();
         }
         
-        private VisualElement SetDisabledGroup(VisualElement target, bool disabled)
+        private VisualElement SetElementDimmed(VisualElement target, bool dimmed)
         {
-            var disabledGroup = target.Query(className: "disabled-group").First();
+            var dimmer = target.Query(className: "element-dimmer").First();
             
-            if (disabledGroup == null && disabled)
+            if (dimmer == null && dimmed)
             {
-                disabledGroup = new VisualElement { style = {
+                dimmer = new VisualElement { style = {
                     position = Position.Absolute,
                     top = 0, left = 0, right = 0, bottom = 0,
                     backgroundColor = isProSkin ? new Color(0.23f, 0.23f, 0.23f, 0.5f) : new Color(0.77f, 0.77f, 0.77f, 0.5f)
                 }};
                 
-                disabledGroup.pickingMode = PickingMode.Ignore;
-                disabledGroup.AddToClassList("disabled-group");
-                target.Add(disabledGroup);
+                dimmer.pickingMode = PickingMode.Ignore;
+                dimmer.AddToClassList("element-dimmer");
+                target.Add(dimmer);
             }
-            else if (disabledGroup != null && !disabled)
+            else if (dimmer != null && !dimmed)
             {
-                disabledGroup.RemoveFromHierarchy();
+                dimmer.RemoveFromHierarchy();
             }
 
-            return disabledGroup;
+            return dimmer;
         }
     }
 }
