0b0c6cbc94039b07c5c49945c4ef29f86b89d66e
Chika Chang <seika850113@gmail.com>
2021-10-09 18:43:43 +0800

    Fix menu items field is null in 2021.1

--- Base/Utils/GenericMenuUtil.cs
+++ Base/Utils/GenericMenuUtil.cs
@@ -10,8 +10,13 @@ namespace AV.Hierarchy
     internal class GenericMenuUtil
     {
         private static readonly Type menuItemType = typeof(GenericMenu).GetNestedType("MenuItem", BindingFlags.NonPublic | BindingFlags.Instance);
-      
+
+#if UNITY_2021_1_OR_NEWER
+        private static readonly FieldInfo menuItemsField = typeof(GenericMenu).GetField("m_MenuItems", BindingFlags.NonPublic | BindingFlags.Instance);
+#else
         private static readonly FieldInfo menuItemsField = typeof(GenericMenu).GetField("menuItems", BindingFlags.NonPublic | BindingFlags.Instance);
+#endif
+
         private static readonly FieldInfo menuItemContent = menuItemType.GetField("content", BindingFlags.Public | BindingFlags.Instance);
         private static readonly FieldInfo menuItemFunc = menuItemType.GetField("func", BindingFlags.Public | BindingFlags.Instance);
         private static readonly FieldInfo menuItemFunc2 = menuItemType.GetField("func2", BindingFlags.Public | BindingFlags.Instance);
@@ -30,14 +35,14 @@ namespace AV.Hierarchy
         {
             return (GenericMenu.MenuFunction)menuItemFunc.GetValue(menuItem);
         }
-        
+
         public static GenericMenu.MenuFunction2 GetFuncWithUserData(object menuItem)
         {
             return (GenericMenu.MenuFunction2)menuItemFunc2.GetValue(menuItem);
         }
-        
+
         public static void SetFunc(object menuItem, GenericMenu.MenuFunction func)
-        { 
+        {
             menuItemFunc.SetValue(menuItem, func);
         }
     }
